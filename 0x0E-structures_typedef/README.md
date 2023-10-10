a177641bfe630b6dda4263eab80e6cd6c75130f0
Uche oliseokonye <au.oliseokonye@gmail.com>
2023-10-10 09:46:51 +0000

    poppy

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+this is my readme on 0x0E-structures typedef project
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,35 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * struct dog - this is a structure that contains information about a dog
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ *
+ * Description: this structure contains information about a dog
+ *	its name, age and its owner's name
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+typedef struct dog dog_t;
+
+
+void print_dog(struct dog *d);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+
+
+#endif
